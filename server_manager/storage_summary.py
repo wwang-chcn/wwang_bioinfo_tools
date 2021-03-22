8d431e024f08b0dc4351f6c2bf0efcabdcc0e399
wwang-chcn <wwang.tongji@gmail.com>
2021-03-22 13:48:28 +0800

    Add: public raw data file checking

--- server_manager/storage_summary.py
+++ server_manager/storage_summary.py
@@ -10,7 +10,7 @@ out = os.popen('du -h -d 1').read()
 
 storage.append(pd.DataFrame({'user':[x[2:] for x in out.split()[1:-2:2]],'total':out.split()[0:-3:2]}))
 
-file_regex_collection = {'sam': '.*\.sam', 'fastq': '.*\.f\(ast\)?q', 'wig': '.*\.wig', 'bdg': '.*\.b\(e\)?dg\(raph\)?', 'public_raw_data': '[SE]RR.*\.f\(ast\)?q.gz'}
+file_regex_collection = {'sam': '.*\.sam', 'fastq': '.*\.f\(ast\)?q', 'wig': '.*\.wig', 'bdg': '.*\.b\(e\)?dg\(raph\)?', 'public_raw_data': '.*[SE]RR[0-9]+\(_[12]\)?\.f\(ast\)?q.gz'}
 for file_regex_label in file_regex_collection:
     file_regex = file_regex_collection[file_regex_label]
     out  = os.popen(f'for i in *; do echo $i; find ./$i -type f -iregex "{file_regex}" -print0 | du -ch --files0-from=- | tail -n 1 ; done').read()
@@ -22,4 +22,4 @@ pprint(storage_use)
 #find . -iregex ".*\.f\(ast\)?q" > fastq.files.txt
 #find . -iregex ".*\.sam" > sam.files.txt
 #find . -iregex ".*\.b\(e\)?dg\(raph\)?" > bdg.files.txt
-#find . -iregex "[SE]RR.*\.f\(ast\)?q.gz" > public_raw_data.files.txt
+#find . -iregex ".*[SE]RR[0-9]+\(_[12]\)?\.f\(ast\)?q.gz" > public_raw_data.files.txt
