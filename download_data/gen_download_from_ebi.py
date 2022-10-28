ea13526d5c316bd5bca92392d75ccd7ceab8b31d
Wen Wang <wwang.tongji@gmail.com>
2022-10-28 23:03:45 +0800

    Add: handle Paired-end sequencing files

--- download_data/gen_download_from_ebi.py
+++ download_data/gen_download_from_ebi.py
@@ -11,25 +11,31 @@ output_file = sys.argv[2]
 
 
 def get_fastq_file_path(row):
+    fastq_file_paths, fastq_files = [], []
     if 'fastq_aspera' in row:
-        fastq_file_path, fastq_file = row.get('fastq_aspera').split(':')[1], row.get('fastq_aspera').rsplit('/',1)[1]
+        for download_link in row.get('fastq_aspera').split(';'):
+            fastq_file_paths.append(download_link.split(':')[1])
+            fastq_files.append(download_link.rsplit('/',1)[1])
     elif 'fastq_ftp' in row:
-        fastq_file_path, fastq_file = row.get('fastq_ftp').split('/', 1)[1], row.get('fastq_ftp').rsplit('/',1)[1]
+        for download_link in row.get('fastq_ftp').split(';'):
+            fastq_file_paths.append(download_link.split('/')[1])
+            fastq_files.append(download_link.rsplit('/',1)[1])
     else:
         return None, None
     
-    return fastq_file_path, fastq_file
+    return fastq_file_paths, fastq_files
 
 
 def gen_download_script(rows):
     for row in rows:
-        fastq_file_path, fastq_file = get_fastq_file_path(row)
-        if not fastq_file_path:
+        fastq_file_paths, fastq_files = get_fastq_file_path(row)
+        if not fastq_file_paths:
             line_text = r'\t'.join(row.values())
             sys.stderr.write(f'Unexpected line: "{line_text}", skip.\n')
             continue
-        yield f'if [ ! -f {fastq_file} ]; then ~/.aspera/connect/bin/ascp -QT -l 300m -P33001 -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:{fastq_file_path} .; fi'
-        yield f'if [ ! -f {fastq_file} ]; then wget ftp://ftp.sra.ebi.ac.uk/{fastq_file_path}; fi'
+        for fastq_file_path, fastq_file in zip(fastq_file_paths, fastq_files):
+            yield f'if [ ! -f {fastq_file} ]; then ~/.aspera/connect/bin/ascp -QT -l 300m -P33001 -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:{fastq_file_path} .; fi'
+            yield f'if [ ! -f {fastq_file} ]; then wget ftp://ftp.sra.ebi.ac.uk/{fastq_file_path}; fi'
 
 
 with open(proj_file) as input_fhd, \
