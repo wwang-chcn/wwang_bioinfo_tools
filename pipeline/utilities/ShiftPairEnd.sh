cfa5a820db4963069814d64561a5a9cbb9df377e
Wen Wang <wwang.tongji@gmail.com>
2022-11-06 15:52:55 +0800

    Fix: shifted fragments starts less than 0

--- pipeline/utilities/ShiftPairEnd.sh
+++ pipeline/utilities/ShiftPairEnd.sh
@@ -13,4 +13,4 @@ awk -v extsize=${2} '
 {
         mid=($2+$3)/2
         printf $1"\t%d\t%d\t"$4"\t"$5"\t"$6"\n", mid-extsize/4, mid+extsize/4
-}' $1 | sort -k1,1 -k2,2g > ${1::(${#1}-4)}_shift.bed
+}' $1 | awk '{if($2>=0) print}' | sort -k1,1 -k2,2g > ${1::(${#1}-4)}_shift.bed
