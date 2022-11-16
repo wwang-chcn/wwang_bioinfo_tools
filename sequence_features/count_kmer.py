d05c48d29eabdf7284581738d7e8f17845713a54
wwang-chcn <wwang.bio@gmail.com>
2022-11-16 13:52:07 +0800

    Update count_kmer.py
    Add: count_kmer2

--- sequence_features/count_kmer.py
+++ sequence_features/count_kmer.py
@@ -27,3 +27,27 @@ def count_kmer(seq, kmer_len, seq_tpye='DNA'):
         kmer_count[kmer] = kmer_count_plus[kmer] + kmer_count_plus[reverse_complement(kmer, alphabet_complementary_dict[seq_tpye])]
     return kmer_count
 
+
+def count_kmer2(seq, kmer_len, seq_tpye='DNA'):
+    """Count the occurance times  k-mer from length 1 to k in sequence."""
+    # pre-processing
+    seq = seq.upper()
+    kmer_count_plus = defaultdict(int)
+    kmer_count = defaultdict(int)
+    # count plus strand
+    #     initial stage
+    for i in range(min(len(seq), kmer_len)):
+        for j in range(1,i+1):
+            kmer_count_plus[seq[i-j:i]] += 1
+        print(i, kmer_count_plus)
+    #     whole sequence
+    for i in range(len(seq)-kmer_len+1):
+        for j in range(kmer_len):
+            kmer_count_plus[seq[i+j:i+kmer_len]] += 1
+        print(i+kmer_len, kmer_count_plus)
+    # count minus strand
+    for i in range(1, kmer_len + 1):
+        for kmer in product(alphabet_dict[seq_tpye], repeat=i):
+            kmer = ''.join(kmer)
+            kmer_count[kmer] = kmer_count_plus[kmer] + kmer_count_plus[reverse_complement(kmer, alphabet_complementary_dict[seq_tpye])]
+    return kmer_count
