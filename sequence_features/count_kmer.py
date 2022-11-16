4f7acecd0fb4281e486476f80421ff6540373d52
wwang-chcn <wwang.bio@gmail.com>
2022-11-16 13:57:02 +0800

    Update count_kmer.py
    Add: reverse_complement parameter

--- sequence_features/count_kmer.py
+++ sequence_features/count_kmer.py
@@ -12,7 +12,7 @@ def reverse_complement(seq, complementary_dict):
     return ''.join([complementary_dict[c] for c in seq[::-1]])
 
 
-def count_kmer(seq, kmer_len, seq_tpye='DNA'):
+def count_kmer(seq, kmer_len, seq_tpye='DNA', reverse_complement=True):
     """Count k-mer occurance times in sequence."""
     # pre-processing
     seq = seq.upper()
@@ -21,14 +21,16 @@ def count_kmer(seq, kmer_len, seq_tpye='DNA'):
     # count plus strand
     for i in range(len(seq)-kmer_len+1):
         kmer_count_plus[seq[i:i+kmer_len]] += 1
-    # count minus strand
-    for kmer in product(alphabet_dict[seq_tpye], repeat=kmer_len):
-        kmer = ''.join(kmer)
-        kmer_count[kmer] = kmer_count_plus[kmer] + kmer_count_plus[reverse_complement(kmer, alphabet_complementary_dict[seq_tpye])]
-    return kmer_count
+    if reverse_complement: # count minus strand
+        for kmer in product(alphabet_dict[seq_tpye], repeat=kmer_len):
+            kmer = ''.join(kmer)
+            kmer_count[kmer] = kmer_count_plus[kmer] + kmer_count_plus[reverse_complement(kmer, alphabet_complementary_dict[seq_tpye])]
+        return kmer_count
+    else:
+        return kmer_count_plus
 
 
-def count_kmer2(seq, kmer_len, seq_tpye='DNA'):
+def count_kmer2(seq, kmer_len, seq_tpye='DNA', reverse_complement=True):
     """Count the occurance times  k-mer from length 1 to k in sequence."""
     # pre-processing
     seq = seq.upper()
@@ -45,9 +47,11 @@ def count_kmer2(seq, kmer_len, seq_tpye='DNA'):
         for j in range(kmer_len):
             kmer_count_plus[seq[i+j:i+kmer_len]] += 1
         print(i+kmer_len, kmer_count_plus)
-    # count minus strand
-    for i in range(1, kmer_len + 1):
-        for kmer in product(alphabet_dict[seq_tpye], repeat=i):
-            kmer = ''.join(kmer)
-            kmer_count[kmer] = kmer_count_plus[kmer] + kmer_count_plus[reverse_complement(kmer, alphabet_complementary_dict[seq_tpye])]
-    return kmer_count
+    if reverse_complement: # count minus strand
+        for i in range(1, kmer_len + 1):
+            for kmer in product(alphabet_dict[seq_tpye], repeat=i):
+                kmer = ''.join(kmer)
+                kmer_count[kmer] = kmer_count_plus[kmer] + kmer_count_plus[reverse_complement(kmer, alphabet_complementary_dict[seq_tpye])]
+        return kmer_count
+    else:
+        return kmer_count_plus
