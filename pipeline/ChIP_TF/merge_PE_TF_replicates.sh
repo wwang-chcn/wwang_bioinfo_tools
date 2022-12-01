28e3c5f32b167eb25edb9782783cd60619e937dd
wwang-chcn <wwang.bio@gmail.com>
2022-12-01 15:48:05 +0800

    Update merge_PE_TF_replicates.sh
    Change sort parameters.
    Correct fragments length calculation method.

--- pipeline/ChIP_TF/merge_PE_TF_replicates.sh
+++ pipeline/ChIP_TF/merge_PE_TF_replicates.sh
@@ -7,7 +7,7 @@ function print_help {
 }
 
 function bedToBigWig {
-    fragment_length=`awk 'BEGIN{s=0} {s+=$2} END{printf "%f", s/NR}' ${name}_fragments.bed`
+    fragment_length=`awk 'BEGIN{s=0} {s+=$3-$2} END{printf "%f", s/NR}' ${name}_fragments.bed`
     ${MY_PATH}/../utilities/ShiftPairEnd.sh ${name}_fragments.bed ${fragment_length} ~/source/bySpecies/${genomeVersion}/${genomeVersion}_main.chrom.sizes
     n=`wc -l ${name}_fragments_shift.bed | cut -f 1 -d " "`
     c=`bc -l <<< "1000000 / $n"`
@@ -21,7 +21,7 @@ function compress_bed {
     genomeVersion=${2}
     col=`head -1 ${bedFile} | awk '{print NF}'`
     plus=`bc <<< "$col -3"`
-    intersectBed -a ${bedFile} -b <(awk '{print $1"\t0\t"$2}' ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes) -wa -f 1.00 | sort -k1,1 -k2,2n > ${bedFile}.tmp
+    intersectBed -a ${bedFile} -b <(awk '{print $1"\t0\t"$2}' ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes) -wa -f 1.00 | sort -S 5% -k1,1 -k2,2n > ${bedFile}.tmp
     bedToBigBed -type=bed3+${plus} ${bedFile}.tmp ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes ${bedFile::(${#bedFile}-2)}b
     rm ${bedFile} ${bedFile}.tmp
 }
@@ -54,7 +54,7 @@ for i in $@; do
 done
 
 # merge fragment files
-cat ${fragments_array[@]} | sort -k1,1 -k2,2n > 4_merged_sample/${name}_fragments.bed
+cat ${fragments_array[@]} | sort -S 5% -k1,1 -k2,2n > 4_merged_sample/${name}_fragments.bed
 cd 4_merged_sample/
 bedToBigWig &
 macs2 callpeak -f BEDPE -t ${name}_fragments.bed -n ${name} -g 1.4e9 -q 0.01 --outdir ./ --keep-dup all 2>&1 >>/dev/null | tee ${name}_MACS.out &
