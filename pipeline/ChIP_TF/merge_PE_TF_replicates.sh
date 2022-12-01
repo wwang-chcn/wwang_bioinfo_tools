16266dccf2d79c5b01db0616a89a16045e10980a
wwang-chcn <wwang.bio@gmail.com>
2022-12-01 15:00:09 +0800

    Update merge_PE_TF_replicates.sh
    Add ShiftPairEnd process.
    Add betToBigWig process.

--- pipeline/ChIP_TF/merge_PE_TF_replicates.sh
+++ pipeline/ChIP_TF/merge_PE_TF_replicates.sh
@@ -7,11 +7,13 @@ function print_help {
 }
 
 function bedToBigWig {
-    n=`wc -l ${name}_fragments.bed | cut -f 1 -d " "`
+    fragment_length=`awk 'BEGIN{s=0} {s+=$2} END{printf "%f", s/NR}' ${name}_fragments.bed`
+    ${MY_PATH}/../utilities/ShiftPairEnd.sh ${name}_fragments.bed ${fragment_length} ~/source/bySpecies/${genomeVersion}/${genomeVersion}_main.chrom.sizes
+    n=`wc -l ${name}_fragments_shift.bed | cut -f 1 -d " "`
     c=`bc -l <<< "1000000 / $n"`
-    genomeCoverageBed -bga -scale $c -i ${name}_fragments.bed -g ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes > ${name}_fragments.bdg
-    ${MY_PATH}/../utilities/bdg2bw.sh ${name}_fragments.bdg ~/source/bySpecies/${genomeVersion}/${genomeVersion}_main.chrom.sizes ${name}
-    rm ${name}_fragments.bdg
+    genomeCoverageBed -bga -scale $c -i ${name}_fragments_shift.bed -g ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes > ${name}_fragments_shift.bdg
+    ${MY_PATH}/../utilities/bdg2bw.sh ${name}_fragments_shift.bdg ~/source/bySpecies/${genomeVersion}/${genomeVersion}_main.chrom.sizes ${name}
+    rm ${name}_fragments_shift.bdg ${name}_fragments_shift.bed
 }
 
 function compress_bed {
@@ -56,6 +58,7 @@ cat ${fragments_array[@]} | sort -k1,1 -k2,2n > 4_merged_sample/${name}_fragment
 cd 4_merged_sample/
 macs2 callpeak -f BEDPE -t ${name}_fragments.bed -n ${name} -g 1.4e9 -q 0.01 --outdir ./ --keep-dup all 2>&1 >>/dev/null | tee ${name}_MACS.out
 compress_bed ${name}_fragments.bed ${genomeVersion}
+bedToBigWig
 cd ..
 
 # compress fragment files
