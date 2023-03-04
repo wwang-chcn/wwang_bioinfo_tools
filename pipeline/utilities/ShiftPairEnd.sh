9a87edf21ae840ca71cd81927a19757e3f86ab1a
wwang-chcn <wwang.bio@gmail.com>
2023-03-04 21:26:30 +0800

    Fix bug for unexpected columns in fragments.bed

--- pipeline/utilities/ShiftPairEnd.sh
+++ pipeline/utilities/ShiftPairEnd.sh
@@ -12,5 +12,5 @@ function print_help {
 awk -v extsize=${2} '
 {
         mid=($2+$3)/2
-        printf $1"\t%d\t%d\t"$4"\t"$5"\t"$6"\n", mid-extsize/4, mid+extsize/4
-}' $1 | awk '{if($2>=0) print; else print $1"\t0\t"$3"\t"$4"\t"$5"\t"$6}' | sort -k1,1 -k2,2n | intersectBed -a - -b <(awk '{print $1"\t0\t"$2}' $3) -wa -f 1.00 > ${1::(${#1}-4)}_shift.bed
+        printf $1"\t%d\t%d\n", mid-extsize/4, mid+extsize/4
+}' $1 | awk '{if($2>=0) print; else print $1"\t0\t"$3}' | sort -k1,1 -k2,2n | intersectBed -a - -b <(awk '{print $1"\t0\t"$2}' $3) -wa -f 1.00 > ${1::(${#1}-4)}_shift.bed
