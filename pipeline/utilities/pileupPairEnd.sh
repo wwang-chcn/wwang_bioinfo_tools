eaccb01458b3ef268ecb0f05182a73c4193d799b
Wen Wang (王文) <wwang.bio@gmail.com>
2024-01-02 17:06:32 -0500

    Add: clean up bdg file

--- pipeline/utilities/pileupPairEnd.sh
+++ pipeline/utilities/pileupPairEnd.sh
@@ -16,4 +16,4 @@ c=`bc -l <<< "1000000 / $n"` && \
 genomeCoverageBed -bga -scale $c -i ${1::(${#1}-4)}_shift.bed -g $2 | awk '{if($3>$2) print $0}' > ${1::(${#1}-4)}.bdg && \
 bedtools intersect -a ${1::(${#1}-4)}.bdg -b <(awk '{print $1"\t0\t"$2}' $2) -wa -f 1.00 | sort -S 5% -k1,1 -k2,2n > ${1}.tmp
 bedGraphToBigWig ${1}.tmp ${2} ${3}.bw
-rm ${1::(${#1}-4)}_shift.bed ${1}.tmp
+rm ${1::(${#1}-4)}_shift.bed ${1}.tmp ${1::(${#1}-4)}.bdg
