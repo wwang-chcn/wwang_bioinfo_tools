bc1d4a1b5492bc94d76ad701991b6767ecd6f2c2
Wen Wang (王文) <wwang.bio@gmail.com>
2024-01-02 13:34:14 -0500

    Update: remove 4th column

--- pipeline/utilities/pileupPairEnd.sh
+++ pipeline/utilities/pileupPairEnd.sh
@@ -9,8 +9,8 @@ function print_help {
 awk '{
     mid=($2+$3)/2
     fraglen=$3-$2
-    printf $1"\t%d\t%d\t%s\n", mid-fraglen/2, mid+fraglen/2, $4
-}' $1 | awk '{if($2>=0) print; else print $1"\t0\t"$3"\t"$4}' | sort -k1,1 -k2,2n | intersectBed -a - -b <(awk '{print $1"\t0\t"$2}' $2) -wa -f 1.00 > ${1::(${#1}-4)}_shift.bed && \
+    printf $1"\t%d\t%d\t%s\n", mid-fraglen/2, mid+fraglen/2
+}' $1 | awk '{if($2>=0) print; else print $1"\t0\t"$3}' | sort -k1,1 -k2,2n | intersectBed -a - -b <(awk '{print $1"\t0\t"$2}' $2) -wa -f 1.00 > ${1::(${#1}-4)}_shift.bed && \
 n=`wc -l ${1::(${#1}-4)}_shift.bed | cut -f 1 -d " "` && \
 c=`bc -l <<< "1000000 / $n"` && \
 genomeCoverageBed -bga -scale $c -i ${1::(${#1}-4)}_shift.bed -g $2 | awk '{if($3>$2) print $0}' > ${1::(${#1}-4)}.bdg && \
