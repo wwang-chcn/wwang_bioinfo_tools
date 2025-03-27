4a1cec7bb20ff67c29e61e5e5dddf0f8953a7552
Wen Wang (王文) <wwang.bio@gmail.com>
2025-03-27 11:36:42 -0400

    Update

--- pipeline/histone/merge_PE_histone_replicates.sh
+++ pipeline/histone/merge_PE_histone_replicates.sh
@@ -22,11 +22,14 @@ function bedToBigWig {
 function compress_bed {
     bedFile=${1}
     genomeVersion=${2}
-    col=`head -1 ${bedFile} | awk '{print NF}'`
-    plus=`bc <<< "$col -3"`
-    intersectBed -a ${bedFile} -b <(awk '{print $1"\t0\t"$2}' ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes) -wa -f 1.00 | sort -k1,1 -k2,2n > ${bedFile}.tmp
-    bedToBigBed -type=bed3+${plus} ${bedFile}.tmp ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes ${bedFile::(${#bedFile}-2)}b
-    rm ${bedFile} ${bedFile}.tmp
+    
+    if [[ ! -e ${bedFile::(${#bedFile}-2)}b ]]; then
+        col=`head -1 ${bedFile} | awk '{print NF}'`
+        plus=`bc <<< "$col -3"`
+        intersectBed -a ${bedFile} -b <(awk '{print $1"\t0\t"$2}' ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes) -wa -f 1.00 | sort -k1,1 -k2,2n > ${bedFile}.tmp
+        bedToBigBed -type=bed3+${plus} ${bedFile}.tmp ~/source/bySpecies/${genomeVersion}/${genomeVersion}.chrom.sizes ${bedFile::(${#bedFile}-2)}b
+        rm ${bedFile} ${bedFile}.tmp
+    fi
 }
 
 function peak_calling {
@@ -53,8 +56,8 @@ function peak_calling {
 
 function clearning_up {
     for i in ${fragments_array[@]}; do
-        if [[ -e 2_signal/${i}_fragments.bb ]]; then
-            rm 2_signal/${i}_fragments.bb
+        if [[ -e 2_signal/${i}_fragments.bed ]]; then
+            rm 2_signal/${i}_fragments.bed
         fi
     done
     if [[ -e 5_merged_sample/${name}_fragments.bed ]]; then
